ae900a46f2415de8f8509f1e7bdd6ce770ffd483
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2023-02-20 16:27:33 +0100

    Include SEIR epidemic model in epidemics benchmark package, and Rosenbrock function in blackbox benchmark package

--- benchmarks/blackbox/__init__.py
+++ benchmarks/blackbox/__init__.py
@@ -20,10 +20,8 @@ class Ackley(Benchmark):
         - e is the euler number
         - a, b, c are three parameter, whose recommended values are a = 20, b = 0.2 and c = 2π
 
-    The function has its minimum in the vector 0_d.
+    The function has its minimum in f(0, ..., 0) = 0.
     """
-
-    _package = "ackley"
 
     _variables = [
         CustomVariable('x', dtype=list, description="the input vector")
@@ -33,7 +31,7 @@ class Ackley(Benchmark):
         NumericParameter('a', default=20., description="the Ackley function 'a' parameter"),
         NumericParameter('b', default=0.2, description="the Ackley function 'b' parameter"),
         NumericParameter('c', default=2 * np.pi, description="the Ackley function 'c' parameter"),
-        NumericParameter('dim', default=1, integer=True, description="the input vector dimension")
+        NumericParameter('dim', default=1, integer=True, lb=1, description="the input vector dimension")
     ]
 
     _constraints = [
@@ -45,7 +43,7 @@ class Ackley(Benchmark):
     ]
 
     @staticmethod
-    def _query(x: list, a: float, b: float, c: float, dim: int) -> np.ndarray:
+    def _query(x, a, b, c, dim):
         x = np.array(x)
         term1 = a * np.exp(-b * np.sqrt(np.sum(x ** 2) / dim))
         term2 = np.exp(np.sum(np.cos(c * x)) / dim)
@@ -56,3 +54,51 @@ class Ackley(Benchmark):
 
     def query(self, x) -> np.ndarray:
         return super(Ackley, self).query(x=x)
+
+
+@benchmark
+class Rosenbrock(Benchmark):
+    """
+    The Rosenbrock function (a.k.a. Rosenbrock's valley or Rosenbrock's banana function) is a non-convex function.
+    It was introduced by Howard H. Rosenbrock in 1960, and it is used as a performance test for optimization algorithms.
+    The global minimum is inside a long, narrow, parabolic shaped flat valley.
+    Hence, to find the valley is trivial, while to converge to the global minimum is difficult.
+
+    It can be computed as 'f(x) = t1 + t2', where:
+        - t1 = Σ_{i=0}^{d-1} [ b * (x_{i+1} - x_i^2)^2 ]
+        - t2 = Σ_{i=0}^{d-1} [ (1 - x_i)^2 ]
+        - d is the dimensionality of the vector x
+        - b is a parameter whose recommended values is b = 100
+
+    The function has its minimum in f(1, ..., 1) = 0.
+    """
+
+    _variables = [
+        CustomVariable('x', dtype=list, description="the input vector")
+    ]
+
+    _parameters = [
+        NumericParameter('b', default=100., description="the Rosenbrock function 'b' parameter"),
+        NumericParameter('dim', default=2, integer=True, lb=2, description="the input vector dimension")
+    ]
+
+    _constraints = [
+        CustomConstraint(
+            name='input_dim',
+            satisfied_fn=lambda x, dim: len(x) == dim,
+            description=f"the input vector should have the correct input dimension 'dim'"
+        )
+    ]
+
+    @staticmethod
+    def _query(x, b, dim):
+        x = np.array(x)
+        term1 = b * (x[1:] - x[:-1] ** 2) ** 2
+        term2 = (1 - x[:-1]) ** 2
+        return np.sum(term1 + term2)
+
+    def __init__(self, name, seed, b, dim):
+        super(Rosenbrock, self).__init__(name=name, seed=seed, b=b, dim=dim)
+
+    def query(self, x) -> np.ndarray:
+        return super(Rosenbrock, self).query(x=x)
--- benchmarks/epidemics/__init__.py
+++ benchmarks/epidemics/__init__.py
@@ -4,20 +4,15 @@ from scipy.integrate import odeint
 
 from benchmarks.benchmark import Benchmark
 from datatypes.constraints import CustomConstraint
-from datatypes.parameters import PositiveParameter, NumericParameter
+from datatypes.parameters import NumericParameter
 from datatypes.variables import PositiveVariable
 from utils.decorators import benchmark
 
 
 @benchmark
-class EpidemicSIR(Benchmark):
+class SIR(Benchmark):
     """
-    Let us assume that we are at the early stages of an epidemic.
-    We want to do our best to control while we wait for a cure/vaccine.
-    Hence, we need to decide which actions to take in order to minimize the total number of infected.
-    But we are subjected to a variety of constraints (e.g. socio-economical impact).
-
-    For our epidemics we will rely on a SIR model, which is the simplest type of compartmental model.
+    Models an epidemics leveraging the SIR model, which is the simplest type of compartmental model.
     The population is divided into three groups (compartments), i.e. Susceptibles (S), Infected (I), and Recovered (R).
     The classical SIR model is dynamic, indeed the size of the three groups evolves over time.
     The evolution of the model is defined by the following system of Ordinary Differential Equations (ODEs):
@@ -27,17 +22,15 @@ class EpidemicSIR(Benchmark):
     with N being the total population size, and 𝛽 and 𝛾 the infection and recovery rate, respectively.
     """
 
-    _package = 'epidemics'
-
     _variables = [
         PositiveVariable('s0', description='the percentage of initial susceptibles'),
         PositiveVariable('i0', description='the percentage of initial infected'),
         PositiveVariable('r0', description='the percentage of initial recovered'),
-        PositiveVariable('beta', strict=True, description='the infection rate')
+        PositiveVariable('beta', strict=True, description='the infection rate'),
+        PositiveVariable('gamma', strict=True, description='the recovery rate')
     ]
 
     _parameters = [
-        PositiveParameter('gamma', default=1. / 14, strict=True, description='the recovery rate'),
         NumericParameter('horizon', default=300, lb=1, integer=True, description='the time horizon of the simulation')
     ]
 
@@ -50,14 +43,12 @@ class EpidemicSIR(Benchmark):
     ]
 
     @staticmethod
-    def _query(s0: float, i0: float, r0: float, beta: float, gamma: float, horizon: int) -> pd.DataFrame:
-        # define sir dynamics
-        def sir(s, i, r):
-            n = sum([s, i, r])
-            ds = -beta * s * i / n
-            di = beta * s * i / n - gamma * i
-            dr = gamma * i
-            return np.array([ds, di, dr])
+    def _query(s0, i0, r0, beta, gamma, horizon):
+        # define sir dynamics (normalization is not necessary since we assume the population to sum up to 1)
+        def sir(s, i, _):
+            t1 = beta * s * i
+            t2 = gamma * i
+            return np.array([-t1, t1 - t2, t2])
 
         # solve ode
         data = odeint(
@@ -70,8 +61,70 @@ class EpidemicSIR(Benchmark):
         data.index.rename("time", inplace=True)
         return data
 
-    def __init__(self, name, seed, gamma, horizon):
-        super(EpidemicSIR, self).__init__(name=name, seed=seed, gamma=gamma, horizon=horizon)
+    def __init__(self, name, seed, horizon):
+        super(SIR, self).__init__(name=name, seed=seed, horizon=horizon)
 
-    def query(self, s0, i0, r0, beta) -> pd.DataFrame:
-        return super(EpidemicSIR, self).query(s0=s0, i0=i0, r0=r0, beta=beta)
+    def query(self, s0, i0, r0, beta, gamma) -> pd.DataFrame:
+        return super(SIR, self).query(s0=s0, i0=i0, r0=r0, beta=beta, gamma=gamma)
+
+
+@benchmark
+class SEIR(Benchmark):
+    """
+    Models an epidemic leveraging the SEIR model.
+    The population is divided into three groups (compartments), i.e. Susceptibles (S), Infected (I), and Recovered (R).
+    The classical SIR model is dynamic, indeed the size of the three groups evolves over time.
+    The evolution of the model is defined by the following system of Ordinary Differential Equations (ODEs):
+        dS = -𝛽 * S * I / N
+        dI = 𝛽 * S * I / N - 𝛾 * I
+        dR = 𝛾 * I
+    with N being the total population size, and 𝛽 and 𝛾 the infection and recovery rate, respectively.
+    """
+
+    _variables = [
+        PositiveVariable('s0', description='the percentage of initial susceptibles'),
+        PositiveVariable('e0', description='the percentage of initial exposed'),
+        PositiveVariable('i0', description='the percentage of initial infected'),
+        PositiveVariable('r0', description='the percentage of initial recovered'),
+        PositiveVariable('beta', strict=True, description='the infection rate'),
+        PositiveVariable('gamma', strict=True, description='the recovery rate'),
+        PositiveVariable('latency', strict=True, description='the exposition latency period')
+    ]
+
+    _parameters = [
+        NumericParameter('horizon', default=300, lb=1, integer=True, description='the time horizon of the simulation')
+    ]
+
+    _constraints = [
+        CustomConstraint(
+            name='percentage',
+            satisfied_fn=lambda s0, e0, i0, r0: s0 + e0 + i0 + r0 == 1,
+            description='the percentages of initial susceptibles, exposed, infected, and recovered must sum up to one'
+        )
+    ]
+
+    @staticmethod
+    def _query(s0, e0, i0, r0, beta, gamma, latency, horizon):
+        # define seir dynamics (normalization is not necessary since we assume the population to sum up to 1)
+        def seir(s, e, i, _):
+            t1 = beta * s * i
+            t2 = latency * e
+            t3 = gamma * i
+            return np.array([-t1, t1 - t2, t2 - t3, t3])
+
+        # solve ode
+        data = odeint(
+            func=lambda y, t: seir(*y),
+            y0=np.array([s0, e0, i0, r0]),
+            t=np.arange(horizon)
+        )
+        # build dataframe
+        data = pd.DataFrame(data=data, index=np.arange(horizon), columns=["S", "E", "I", "R"])
+        data.index.rename("time", inplace=True)
+        return data
+
+    def __init__(self, name, seed, horizon):
+        super(SEIR, self).__init__(name=name, seed=seed, horizon=horizon)
+
+    def query(self, s0, e0, i0, r0, beta, gamma, latency) -> pd.DataFrame:
+        return super(SEIR, self).query(s0=s0, e0=e0, i0=i0, r0=r0, beta=beta, gamma=gamma, latency=latency)
